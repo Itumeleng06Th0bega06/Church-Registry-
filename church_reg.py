3fd5ae8b68235b41650b5d5a9be03dbdbac4b4d2
Itumeleng06Th0bega06 <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2024-08-24 09:20:42 +0200

    Add files via upload

--- church_reg.py
+++ church_reg.py
@@ -7,13 +7,13 @@ col1,col2=st.columns([2,1])
 
 with col1:
     with st.form("form", clear_on_submit=True):
-        star=st.markdown(":blue[required] "'*')
-        name=st.text_input(":blue[Name and Surname]"' *')
-        place=st.text_input(":blue[Place]"'   *')
-        gender=st.radio(":blue[Gender]"'  *',["Male","Female"])
-        att=st.multiselect(":blue[Select all days you will be Attending?]",["Thurs","Fri","Sat","Sunday"])
-        family=st.text_input(":blue[Please Indicate how many Children are you coming with.]")
-        contact=st.number_input(":blue[Contacts]"' *', value=None, step=1, placeholder="Only ten digits")
+        star=st.markdown(":orange[required] "'*')
+        name=st.text_input(":orange[Name and Surname]"' *')
+        place=st.text_input(":orange[Place]"'   *')
+        gender=st.radio(":orange[Gender]"'  *',["Male","Female"])
+        att=st.multiselect(":orange[Select all days you will be Attending?]",["Thurs","Fri","Sat","Sunday"])
+        family=st.text_input(":orange[Please Indicate how many Children are you coming with.]")
+        contact=st.number_input(":orange[Contacts]"' *', value=None, step=1, placeholder="Ten digits")
         submit=st.form_submit_button("Register", help="!!Please Make Sure Your information is correct before saving!!")
         # we initialize an empty storage
         if 'all_members_info' not in st.session_state:
